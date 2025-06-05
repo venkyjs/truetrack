f8e1934fc5855c6a24e7bb43c51e7ddc5e5b8046
Venkat <hellovenkat@gmail.com>
2025-06-05 17:12:25 -0400

    Single-file output

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import '@fortawesome/fontawesome-svg-core/styles.css';
+import faviconUrl from './assets/favicon.ico';
+
+// Dynamically set the favicon
+const faviconLink = document.createElement('link');
+faviconLink.rel = 'icon';
+faviconLink.href = faviconUrl;
+document.head.appendChild(faviconLink);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
