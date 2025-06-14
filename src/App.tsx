22b9dedcf41160b1ad49cbdc087a31b50c3ab9c9
Venkat <hellovenkat@gmail.com>
2025-06-13 20:07:44 -0400

    Update grid layout configuration in App component and add CSS overrides for react-grid-layout placeholder styling

--- src/App.tsx
+++ src/App.tsx
@@ -395,8 +395,8 @@ const App: FC = () => {
                         className='layout'
                         layouts={layouts}
                         breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
-                        cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
-                        rowHeight={30}
+                        cols={{ lg: 4, md: 4, sm: 2, xs: 1, xxs: 1 }}
+                        rowHeight={20}
                         draggableHandle='.project-drag-handle'
                         onLayoutChange={handleLayoutChange}
                     >
